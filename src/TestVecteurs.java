f90f9b95915b0ff74370c705f9123e9433cf3826
kheirou98 <khe-98@hotmail.com>
2017-10-20 16:51:41 +0100

    Final commit

--- src/TestVecteurs.java
+++ src/TestVecteurs.java
@@ -5,11 +5,6 @@ import java.util.ArrayList;
 import org.junit.Assert;
 import org.junit.Test;
 
-<<<<<<< HEAD
-
-
-
-
 public class TestVecteurs {
 	/**
 	 * testtritab :est une méthode qui fait le teste unitaire de la methode tritab.
@@ -53,9 +48,7 @@ public class TestVecteurs {
 	    ArrayList <Long> result = new ArrayList <Long>();
 		Vecteurs test = new Vecteurs(tab) ;
 		result = test.inverse(tab);
-=======
-public class TestVecteurs {
-
+	}
 	@Test
     public void testmin(){
 		ArrayList <Long> tab= new ArrayList (){{add((long)19);add((long)1);add((long)88);add((long)12);add((long)3);}};
@@ -81,15 +74,11 @@ public class TestVecteurs {
 	    ArrayList <Long> result = new ArrayList <Long>();
 		Vecteurs test = new Vecteurs(tab) ;
 		result = test.facto_tab(tab);
->>>>>>> Kbranch
 		Object[] resulttest = result.toArray();
 		Object[] tab2test = tab2.toArray();
 		Assert.assertArrayEquals(tab2test,resulttest);		
     }
-<<<<<<< HEAD
-}
-=======
-	
+
 	
 }
->>>>>>> Kbranch
+
--- src/Vecteurs.java
+++ src/Vecteurs.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
 import java.util.Collections;
-<<<<<<< HEAD
-
-=======
+/**
+ * Cette classe consiste à réaliser quelques applications 
+ * sur les vecteurs telles que le tri des vecteurs, la somme de
+ * deux vecteurs de meme taille bien évidemment , générer l'inverse
+ * d'un vecteur, fournir une formule pour les élements d'un vecteur et
+ * extraire du vecteur la valeur maximale et minimale.
+ *@author MEDKOUR Kheir Eddine et ZINE Hocine.
+ *@version 1.0
+ */
 public class Vecteurs {
 		private ArrayList<Long> vecteur;
 		/**
@@ -21,6 +27,13 @@ public class Vecteurs {
 		 */
 		public ArrayList<Long> getVecteur() {
 			return vecteur;
+		}
+		/**
+		 *setVecteur :(ArrayList(long)) est une méthode qui affecteur un vecteur à celui de la classe(setter).
+		 * @param vecteur est un tableau dynamique.
+		 */
+		public void setVecteur(ArrayList<Long> vecteur) {
+			this.vecteur = vecteur;
 		}
 		public Long Min (ArrayList<Long> tab) {
 			long k = Collections.min(tab);
@@ -70,45 +83,7 @@ public class Vecteurs {
 			facto=inter;
 			return facto;
 		}
-			
 		
->>>>>>> Kbranch
-
-/**
- * Cette classe consiste à réaliser quelques applications 
- * sur les vecteurs telles que le tri des vecteurs, la somme de
- * deux vecteurs de meme taille bien évidemment , générer l'inverse
- * d'un vecteur, fournir une formule pour les élements d'un vecteur et
- * extraire du vecteur la valeur maximale et minimale.
- *@author MEDKOUR Kheir Eddine et ZINE Hocine.
- *@version 1.0
- */
-public class Vecteurs {
-	private ArrayList<Long> vecteur;
-	/**
-	 * Vecteurs est un constructeur.
-	 * Il construit un vecteur qui contient des nombres entiers (Long)
-	 * @param vecteur c'est un tableau dynamique (vecteur)qui va contenir les valeurs 
-	 */
-	
-	public Vecteurs (ArrayList<Long> vecteur){
-		this.vecteur=vecteur;
-		
-	}
-	/**
-	 * getVecteur :est une méthode qui retourne le vecteur de la classe (getter).
-	 * @return vecteur le tableau dynamique.
-	 */
-	public ArrayList<Long> getVecteur() {
-		return vecteur;
-	}
-	/**
-	 *setVecteur :(ArrayList(long)) est une méthode qui affecteur un vecteur à celui de la classe(setter).
-	 * @param vecteur est un tableau dynamique.
-	 */
-	public void setVecteur(ArrayList<Long> vecteur) {
-		this.vecteur = vecteur;
-	}
 	/**
 	 *triTab :(ArrayList(long)) est méthode qui sert à trier un tableau dynamique
 	 * en faisant des permutations entre les élements du tableau
